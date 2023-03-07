e0e209101c8f12fd656c5de554fb149d2811baa8
r-maiwald <r.maiwald@stud.uni-heidelberg.de>
2023-03-07 13:13:23 +0100

    test: add unit test for solve

--- tests/test_pyinverse.py
+++ tests/test_pyinverse.py
@@ -1,1 +1,10 @@
 from pyinverse import pyinverse
+import numpy as np
+
+def test_solve():
+    # Create forward model
+    x = np.array([1,2])
+    K = np.array([[1, 0], [0, 2]])
+    y = K @ x
+    x_solver = pyinverse.solve(y, K)
+    assert np.allclose(x, x_solver)
